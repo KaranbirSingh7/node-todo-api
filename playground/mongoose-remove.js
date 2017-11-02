8f5e82d46059eac7f400d0c3879cd3a326491813
Karanbir Singh <31749546+KaranbirSingh7@users.noreply.github.com>
2017-11-02 15:32:08 -0400

    Add DELETE /todos/:id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,11 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+// Todo.remove({}).then((res) => {
+//     console.log(res);
+// });
+
+// Todo.findByIdAndRemove('59fb6c13ae0d700cb4450a08').then((res) => {
+//     console.log(res);
+// });
+
--- server/server.js
+++ server/server.js
@@ -51,6 +51,24 @@ app.get('/todos/:id', (req, res) => {
     }
 })
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    } else {
+        Todo.findByIdAndRemove(id).then((todo) => {
+            if (!todo) {
+                res.status(404).send();
+            } else {
+                res.status(200).send(todo);
+            }
+        }, (err) => {
+            res.status(400).send();
+        });
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port', port);
 });
